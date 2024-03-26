af978d0fdcd4fb6fdcd186192113fcbf2602d617
Ítalo Rezende <italo.rezende@sharkdev.com.br>
2024-03-25 23:49:15 -0300

    feat: schedule layout

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,42 @@
+import { Calendar } from '@/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+import { useState } from 'react'
+
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const isDateSelected = !!selectedDate
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelect={setSelectedDate} />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00 h</TimePickerItem>
+            <TimePickerItem>09:00 h</TimePickerItem>
+            <TimePickerItem>10:00 h</TimePickerItem>
+            <TimePickerItem>11:00 h</TimePickerItem>
+            <TimePickerItem>12:00 h</TimePickerItem>
+            <TimePickerItem>13:00 h</TimePickerItem>
+            <TimePickerItem>14:00 h</TimePickerItem>
+            <TimePickerItem>15:00 h</TimePickerItem>
+            <TimePickerItem>16:00 h</TimePickerItem>
+            <TimePickerItem>17:00 h</TimePickerItem>
+            <TimePickerItem>18:00 h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
